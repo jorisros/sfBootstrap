31adb602ca86556f8ab0a6c516c705a940b7244d
Joris Ros <joris.ros@gmail.com>
2015-03-22 22:43:09 +0100

    Add styling for flashes

--- data/generator/sfPropelModule/bootstrap/template/templates/_flashes.php
+++ data/generator/sfPropelModule/bootstrap/template/templates/_flashes.php
@@ -1,7 +1,13 @@
+<div class="container">
 [?php if ($sf_user->hasFlash('notice')): ?]
-  <div class="notice">[?php echo __($sf_user->getFlash('notice'), array(), 'sf_admin') ?]</div>
-[?php endif; ?]
+    <div class="alert alert-warning" role="alert">
+        <div class="notice">[?php echo __($sf_user->getFlash('notice'), array(), 'sf_admin') ?]</div>
+    </div>
+ [?php endif; ?]
 
 [?php if ($sf_user->hasFlash('error')): ?]
-  <div class="error">[?php echo __($sf_user->getFlash('error'), array(), 'sf_admin') ?]</div>
+    <div class="alert alert-danger" role="alert">
+        <div class="error">[?php echo __($sf_user->getFlash('error'), array(), 'sf_admin') ?]</div>
+    </div>
 [?php endif; ?]
+</div>
