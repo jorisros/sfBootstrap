b061b5407e3c8a7688c9fab2e9f2e8d27576d12e
Joris Ros <joris.ros@gmail.com>
2015-03-22 22:47:59 +0100

    add icon

--- data/generator/sfPropelModule/bootstrap/template/templates/_flashes.php
+++ data/generator/sfPropelModule/bootstrap/template/templates/_flashes.php
@@ -1,13 +1,13 @@
 <div class="container">
 [?php if ($sf_user->hasFlash('notice')): ?]
     <div class="alert alert-warning" role="alert">
-        <div class="notice">[?php echo __($sf_user->getFlash('notice'), array(), 'sf_admin') ?]</div>
+        <div class="notice"><span class="glyphicon glyphicon-exclamation-sign">&nbsp;[?php echo __($sf_user->getFlash('notice'), array(), 'sf_admin') ?]</div>
     </div>
  [?php endif; ?]
 
 [?php if ($sf_user->hasFlash('error')): ?]
     <div class="alert alert-danger" role="alert">
-        <div class="error">[?php echo __($sf_user->getFlash('error'), array(), 'sf_admin') ?]</div>
+        <div class="error"><span class="glyphicon glyphicon-exclamation-sign"></span>&nbsp;[?php echo __($sf_user->getFlash('error'), array(), 'sf_admin') ?]</div>
     </div>
 [?php endif; ?]
 </div>
