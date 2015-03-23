9ba102b0c4c2321d54daa8f8aea1496973ca9b90
Joris Ros <joris.ros@gmail.com>
2015-03-23 08:55:05 +0100

    styling of the filters in list

--- data/generator/sfPropelModule/bootstrap/template/templates/_filters.php
+++ data/generator/sfPropelModule/bootstrap/template/templates/_filters.php
@@ -1,24 +1,20 @@
 [?php use_stylesheets_for_form($form) ?]
 [?php use_javascripts_for_form($form) ?]
-<form class="form-horizontal" action="[?php echo url_for('<?php echo $this->getUrlForAction('collection') ?>', array('action' => 'filter')) ?]" method="post">
+<div class="container">
+<form class="form-inline" action="[?php echo url_for('<?php echo $this->getUrlForAction('collection') ?>', array('action' => 'filter')) ?]" method="post">
 
-<div class="form-group">
+
   [?php if ($form->hasGlobalErrors()): ?]
     [?php echo $form->renderGlobalErrors() ?]
   [?php endif; ?]
 
-      <table cellspacing="0">
-      <tfoot>
-        <tr>
-          <td colspan="2">
+            <div class="form-group">
             [?php echo $form->renderHiddenFields() ?]
-            [?php echo link_to(__('Reset', array(), 'sf_admin'), '<?php echo $this->getUrlForAction('collection') ?>', array('action' => 'filter'), array('query_string' => '_reset', 'method' => 'post')) ?]
+              [?php //echo link_to(__('Reset', array(), array('class'=>'test')), '<?php echo $this->getUrlForAction('collection') ?>', array('action' => 'filter'), array('query_string' => '_reset', 'method' => 'post')) ?]
             <input type="submit" class="btn btn-default" value="[?php echo __('Filter', array(), 'sf_admin') ?]" />
-          </td>
-        </tr>
-      </tfoot>
-      <tbody>
+</div>
         [?php foreach ($configuration->getFormFilterFields($form) as $name => $field): ?]
+
         [?php if ((isset($form[$name]) && $form[$name]->isHidden()) || (!isset($form[$name]) && $field->isReal())) continue ?]
           [?php include_partial('<?php echo $this->getModuleName() ?>/filters_field', array(
             'name'       => $name,
@@ -29,9 +25,9 @@
             'field'      => $field,
             'class'      => 'sf_admin_'.strtolower($field->getType()).' sf_admin_filter_field_'.$name,
           )) ?]
-        [?php endforeach; ?]
-      </tbody>
-    </table>
 
-</div>
+        [?php endforeach; ?]
+
+
 </form>
+</div>
